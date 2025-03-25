eb19717786ce6077ea04f58807adbffcab39c082
LelioMarcos <leliomrcunha@gmail.com>
2025-03-25 14:06:52 -0300

    halve proportions

--- objects/models/primitives.py
+++ objects/models/primitives.py
@@ -2,6 +2,7 @@ def square(w, h, x=0, y=0):
     return rectangle(w, h, 0, x, y, 0)
 
 def rectangle(w, h, d, x=0, y=0, z=0):
+    w, h, d = w/2, h/2, d/2
     return [(-w + x, -h + y, +d + z),
             (+w + x, -h + y, +d + z),
             (-w + x, +h + y, +d + z),
