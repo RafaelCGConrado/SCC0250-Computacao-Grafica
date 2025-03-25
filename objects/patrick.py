a20542c65cbdfff5a48c3646b8211591d4a1579e
Rafael Conrado <106274045+RafaelCGConrado@users.noreply.github.com>
2025-03-25 15:27:02 -0300

    Update patrick.py

--- objects/patrick.py
+++ objects/patrick.py
@@ -6,7 +6,7 @@ from .models.patrick import patrick
 
 class Patrick(object3d.Object3d):
     def __init__(self):
-        super().__init__(model=patrick, scale=0.3)
+        super().__init__(model=patrick, scale=0.3, position=[0,0,0], angles=[0,0,0])
 
     def key_event(self, key):
         if (key==10):
