c0574bd55fc37478ea54de033a2cfa2ae7743eca
LelioMarcos <leliomrcunha@gmail.com>
2025-04-04 21:04:05 -0300

    Fix (ig) the bubbles

--- objects/bubble.py
+++ objects/bubble.py
@@ -9,17 +9,19 @@ class Bubble(object3d.Object3d):
     #É possível escolher a posição de cada coral por meio dos parâmetros
     def __init__(self, velocity):
         self.x_vary = 0
+        self.x_init = np.random.uniform(-1, 1)
         self.velocity = velocity
         super().__init__(
             model=bubble,
-            position=[np.random.uniform(-1, 1), np.random.uniform(-1, 1), -1],
+            position=[self.x_init, np.random.uniform(-1, 1), -1],
         )
     def draw(self, program, loc_color, delta_time):
         self.position[1] += self.velocity*delta_time
         self.x_vary += 2*delta_time
         if self.position[1] >= 1.5:
             self.position[1] = -1
-            self.position[0] = np.random.uniform(-1, 1)
-        self.position[0] += 0.005*np.sin(self.x_vary) 
+            self.x_init = np.random.uniform(-1, 1)
+            self.position[0] = self.x_init
+        self.position[0] = 0.2*np.sin(self.x_vary) + self.x_init
         super().draw(program, loc_color, delta_time, draw_type=GL_TRIANGLE_FAN)
 
